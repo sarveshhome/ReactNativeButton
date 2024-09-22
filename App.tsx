37a9a46dff60eec5ef7d114a8da00a4cd81c4216
sarveshhome <sarvesh.carlton@gmail.com>
2024-09-22 16:14:03 +0530

    button and textbox

--- App.tsx
+++ App.tsx
@@ -12,7 +12,7 @@ import {
 
 const App = () => {
   const [email, setEmail] = useState('');
-  const [password, setPassword]= useState('');
+  const [password, setPassword] = useState('');
   const handlePress = () => {
     console.log('Button pressed!');
   };
@@ -20,34 +20,34 @@ const App = () => {
   return (
     <SafeAreaView>
       <ScrollView>
-        <TextInput 
+        <TextInput
           value={email}
           keyboardType={'email-address'}
           style={{borderWidth: 1, borderRadius: 4, padding: 10}}
           placeholder={'Please enter your email here'}
-          onChangeText={value=>{
-            setEmail(value)
-          }}
-          ></TextInput>
-           <TextInput 
-           value={password}
+          onChangeText={value => {
+            setEmail(value);
+          }}></TextInput>
+        <TextInput
+          value={password}
           secureTextEntry={true}
           style={{borderWidth: 1, borderRadius: 4, padding: 10}}
           placeholder={'Please enter your password here'}
-          onChangeText={value=>{
-            setPassword(value)
-          }}
-          ></TextInput>
-          <Pressable style={{backgroundColor:'black'}}
-           disabled={email.length===0 || password.length<8}
-           onPress={()=>{
+          onChangeText={value => {
+            setPassword(value);
+          }}></TextInput>
+        <Pressable
+          style={{backgroundColor: 'black'}}
+          disabled={email.length === 0 || password.length < 8}
+          onPress={() => {
             console.log('pressed');
-            console.log(email,password);
-          }}
-          > 
-            <Text style={{color: 'white', textAlign: 'center', padding:10 }}>Submit</Text>
-          </Pressable>
-          {/* <Button title={'Submit'} color={'red'} style={{backgroundColor: 'black'}} ></Button> */}
+            console.log(email, password);
+          }}>
+          <Text style={{color: 'white', textAlign: 'center', padding: 10}}>
+            Submit
+          </Text>
+        </Pressable>
+        {/* <Button title={'Submit'} color={'red'} style={{backgroundColor: 'black'}} ></Button> */}
       </ScrollView>
     </SafeAreaView>
     // <View style={styles.container}>
